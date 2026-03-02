89c0d212316ee2a4156496783de84b4d3af2a8ba
vijaykj1526 <vijaykj1526@gmail.com>
2026-03-02 16:58:16 +0530

    Fix analyze issues for CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
+import 'package:cicd/presentation/screens/user_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cicd/presentation/screens/user_screen.dart';
 
 void main() {
   testWidgets('UserScreen loads successfully', (WidgetTester tester) async {
